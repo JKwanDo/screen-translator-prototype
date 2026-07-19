4440195506946e9c96aae06bb4b255f3b96b9c1f
JKwanDo <127279093+JKwanDo@users.noreply.github.com>
2026-07-18 20:12:35 -0400

    relocated file

--- obj/Debug/net8.0/screen-translator-prototype.AssemblyInfo.cs
+++ obj/Debug/net8.0/screen-translator-prototype.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("screen-translator-prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4948942f8bd32141e98820143a670dbd613bde9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65630ab4d07cf43308dc659576d658386aec0e20")]
 [assembly: System.Reflection.AssemblyProductAttribute("screen-translator-prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("screen-translator-prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
