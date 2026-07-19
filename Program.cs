450244f04a79686e73302f2a4e267df3e6fe3aee
JKwanDo <127279093+JKwanDo@users.noreply.github.com>
2026-07-18 20:52:34 -0400

    raised timer to 60s as to not fill memory instantly. Bitmap is now converted into pix to be processed by Tesseract.

--- Program.cs
+++ Program.cs
@@ -6,26 +6,42 @@ using System.IO;
 using System.Drawing;
 
 
-TesseractEngine engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+TesseractEngine engine = new TesseractEngine(@"./tessdata", "rus", EngineMode.Default);
+Directory.CreateDirectory("tessoutput");
 Stopwatch timer = new Stopwatch();
-while (true)
+try
 {
-
-    timer.Restart();
-
-    Rectangle bounds = new Rectangle(0, 0, 1920, 1080);
-
-    using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+    while (true)
     {
-        using (Graphics g = Graphics.FromImage(bitmap))
+        timer.Restart();
+
+        Rectangle bounds = new Rectangle(0, 0, 1920, 1080);
+
+        using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
         {
-        }
-        // 0.5 fps screenCapture loop
-        int elapsedTimeMs = (int)timer.ElapsedMilliseconds;
-        int waitTime = 2000 - elapsedTimeMs;
-        if (waitTime > 0)
-        {
-            Thread.Sleep(waitTime);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(bounds.Left, bounds.Top, 0, 0, bounds.Size);
+            }
+            using (var page = engine.Process(bitmap))
+            {
+                string text = page.GetText();
+                var result = new { timestamp = DateTime.Now, text };
+                string json = System.Text.Json.JsonSerializer.Serialize(result, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText("tessoutput/output.json", json);
+            }
+            // 0.5 fps screenCapture loop
+            int elapsedTimeMs = (int)timer.ElapsedMilliseconds;
+            int waitTime = 60000 - elapsedTimeMs;
+            if (waitTime > 0)
+            {
+                Thread.Sleep(waitTime);
+            }
         }
     }
+}
+finally
+{
+    foreach (var file in Directory.GetFiles("tessoutput"))
+        File.Delete(file);
 }
